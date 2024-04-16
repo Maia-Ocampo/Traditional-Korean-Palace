10eee6a38acc569c151d1f06334b57e5341de8b6
eslamelprince <81124226+eslamelprince@users.noreply.github.com>
2024-04-16 15:38:21 -0400

    layout whitebox

---
+++ Korean Palace/Assets/scripts/FirstPersonMovement.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readytoJump;
+
+    [Header("Keybinds")]
+    public KeyCode JumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    bool grounded = true;
+    public Transform groundcheck;
+
+    public Transform Orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readytoJump = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        print("bruh");
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (grounded)
+        {
+            grounded = false;
+        }
+    }
+
+    private void Update()
+    {
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else    
+        {
+            rb.drag = 0;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(JumpKey) && readytoJump && grounded)
+        {
+            readytoJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = Orientation.forward * verticalInput + Orientation.right * horizontalInput;
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readytoJump = true;
+    }
+}
